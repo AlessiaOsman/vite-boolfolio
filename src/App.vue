61e774450d8161f6bc1c94712b30ee786af842ee
AlessiaOsman <alessia.osman@gmail.com>
2024-03-28 18:41:07 +0100

    milestone 4 completed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <script>
 import axios from 'axios';
-import AppHeader from '../src/components/AppHeader.vue'
-  export default{
+import AppHeader from '../src/components/AppHeader.vue';
+const endpoint = 'http://localhost:8000/api/projects/';
+export default{
     name: 'Boolfolio',
-    components: {AppHeader}
+    components: {AppHeader},
+    data: () => ({
+      projects: []
+    }),
+    methods: {
+      fetchProjects(){
+        axios.get(endpoint).then( res =>{
+          this.projects = res.data
+        })
+      }
+    },
+    created(){
+      this.fetchProjects();
+    }
   }
 </script>
 
 <template>
   <AppHeader/>
+  <main class="container mt-5">
+    <h1>I miei progetti</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+    </ul>
+  </main>
 </template>
 
 <style lang="scss">

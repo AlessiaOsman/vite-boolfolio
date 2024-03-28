e75aa19b6d27a4edc937d0832324702a67586178
AlessiaOsman <alessia.osman@gmail.com>
2024-03-28 18:12:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import axios from 'axios';
+import AppHeader from '../src/components/AppHeader.vue'
+  export default{
+    name: 'Boolfolio',
+    components: {AppHeader}
+  }
+</script>
+
+<template>
+  <AppHeader/>
+</template>
+
+<style lang="scss">
+  @use'./assets/scss/style.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Boolfolio</span>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
